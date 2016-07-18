7f9bbb32734b93e62c4e386b5ac3d0dcab0291ab
sharkdp <davidpeter@web.de>
2016-07-18 16:31:29 +0200

    Add pow, abs. Clean up documentation

--- src/Data/Quantity.purs
+++ src/Data/Quantity.purs
@@ -1,67 +1,92 @@
 module Data.Quantity
-  ( Quantity(..)
+  ( Quantity()
+  , quantity
   , (.*)
-  , approximatelyEqual
-  , dUnit
+  , derivedUnit
+  , pow
+  , abs
   , toSI
+  , approximatelyEqual
   ) where
 
 import Prelude
 
 import Data.Tuple (Tuple(..))
 
-import Data.DerivedUnit (DerivedUnit, toString)
+import Data.DerivedUnit (DerivedUnit, toString, (.^))
 import Data.DerivedUnit as D
 
-import Math (abs)
+import Math as Math
 
--- | A physical quantity.
+-- | Representation of a physical quantity as a (product of a) numerical value
+-- | and a physical unit.
 data Quantity = Quantity Number DerivedUnit
 
-infix 3 Quantity as .*
+-- Used only internally for pattern matching
+infix 3 Quantity as .*.
+
+-- | A smart constructor for physical quantities.
+quantity :: Number → DerivedUnit → Quantity
+quantity = Quantity -- note that we define `quantity` because we do not want
+                    -- to export the `Quantity` constructor. This would leak
+                    -- the internal representation and the bare numerical
+                    -- values.
+
+infix 3 quantity as .*
 
 instance eqQuantity :: Eq Quantity where
-  eq q1 q2 = value q1' == value q2' && dUnit q1' == dUnit q2'
+  eq q1 q2 = value q1' == value q2' && derivedUnit q1' == derivedUnit q2'
     where
       q1' = toSI q1
       q2' = toSI q2
 
-
 instance showQuantity :: Show Quantity where
-  show (num .* unit) = show num <> toString unit
+  show (Quantity num unit) = show num <> toString unit
 
+-- | The numerical value stored inside a `Quantity`. For internal use only
+-- | (bare `Number`s without units should be handled with care).
+value :: Quantity → Number
+value (v .*. _) = v
+
+-- | The unit of a physical quantity.
+derivedUnit :: Quantity → DerivedUnit
+derivedUnit (_ .*. u) = u
+
+-- | Convert a quantity to its SI representation.
+toSI :: Quantity → Quantity
+toSI (num .*. du) =
+  case D.toSI du of
+    Tuple du' conversion -> (conversion * num) .* du'
+
+-- | Raise a quantity to a given power.
+pow :: Quantity → Number → Quantity
+pow (val .*. u) exp = (val `Math.pow` exp) .* (u .^ exp)
+
+-- | The absolute value of a quantity.
+abs :: Quantity → Quantity
+abs (val .*. u) = Math.abs val .* u
+
+-- | Check whether two quantities have matching units (or can be converted
+-- | to the same representation) and test if the numerical are approximately
+-- | equal.
 approximatelyEqual :: Number → Quantity → Quantity → Boolean
 approximatelyEqual tol q1' q2' =
-  dUnit q1 == dUnit q2 &&
-  abs (v1 - v2) < tol * abs (v1 + v2)
+  derivedUnit q1 == derivedUnit q2 &&
+  Math.abs (v1 - v2) < tol * Math.abs (v1 + v2)
     where
       q1 = toSI q1'
       q2 = toSI q2'
       v1 = value q1
       v2 = value q2
 
--- | The numerical value stored inside a `Quantity`. For internal use only
--- | (bare `Number`s without units should be handled with care).
-value :: Quantity → Number
-value (v .* _) = v
+{-- data UnificationError = UnificationError DerivedUnit DerivedUnit --}
 
-dUnit :: Quantity → DerivedUnit
-dUnit (_ .* u) = u
-
-data UnificationError = UnificationError DerivedUnit DerivedUnit
-
-instance showUnificationError :: Show UnificationError where
-  show (UnificationError u1 u2) = "Cannot unify unit '" <> toString u1 <>
-                                  "' with unit '" <> toString u2 <> "'"
-
-
-toSI :: Quantity → Quantity
-toSI (num .* du) =
-  case D.toSI du of
-    Tuple du' conversion -> (conversion * num) .* du'
+{-- instance showUnificationError :: Show UnificationError where --}
+{--   show (UnificationError u1 u2) = "Cannot unify unit '" <> toString u1 <> --}
+{--                                   "' with unit '" <> toString u2 <> "'" --}
 
 {-- convert :: Quantity → DerivedUnit → Either UnificationError Quantity --}
-{-- convert q@(v .* u) u' --}
+{-- convert q@(v .*. u) u' --}
 {--   | u == u' = Right q --}
 {--   | otherwise = --}
 {--       let q' = toSI q --}
