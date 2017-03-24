3990c578f2f5c400720a43bebdd88b3bf51dbc5b
sharkdp <davidpeter@web.de>
2017-03-24 09:09:44 +0100

    Fix identifiers for physical constants

--- src/Data/Quantity/Physics.purs
+++ src/Data/Quantity/Physics.purs
@@ -5,18 +5,20 @@ module Data.Quantity.Physics
   , ℏ
   , electronMass
   , electronCharge
-  , µ₀
-  , ε₀
-  , μ_B
+  , µ0
+  , ε0
+  , µB
   , α
   , protonMass
   , avogadroConstant
+  , kB
+  , g0
   ) where
 
 import Prelude
 
 import Data.Decimal (fromNumber)
-import Data.Quantity ((⊗), (⊘), (.*), Quantity, scalar, pow)
+import Data.Quantity ((⊗), (⊘), (.*), Quantity, scalar, pow, fullSimplify)
 import Data.Quantity.Math (pi)
 import Data.Units ((.^), (./), kilo)
 import Data.Units.SI (meter, second, gram, ampere, kilogram, mole, kelvin)
@@ -47,21 +49,21 @@ electronCharge ∷ Quantity
 electronCharge = 1.60217653e-19 .* coulomb
 
 -- | Magnetic constant (vacuum permeability).
-µ₀ ∷ Quantity
-µ₀ = pi ⊗ (4.0e-7 .* newton ./ ampere .^ 2.0)
+µ0 ∷ Quantity
+µ0 = pi ⊗ (4.0e-7 .* newton ./ ampere .^ 2.0)
 
 -- | Electric constant (vacuum permittivity).
-ε₀ ∷ Quantity
-ε₀ = scalar 1.0 ⊘ (µ₀ ⊗ (speedOfLight `pow` (fromNumber 2.0)))
+ε0 ∷ Quantity
+ε0 = scalar 1.0 ⊘ (µ0 ⊗ (speedOfLight `pow` (fromNumber 2.0)))
 
 -- | Bohr magneton.
-μ_B ∷ Quantity
-μ_B = 9.274009994e-24 .* (joule ./ tesla)
+µB ∷ Quantity
+µB = 9.274009994e-24 .* (joule ./ tesla)
 
 -- | Fine structure constant.
 α ∷ Quantity
-α = electronCharge `pow` (fromNumber 2.0) ⊘
-      (scalar 4.0 ⊗ pi ⊗ ε₀ ⊗ ℏ ⊗ speedOfLight)
+α = fullSimplify $ electronCharge `pow` (fromNumber 2.0) ⊘
+      (scalar 4.0 ⊗ pi ⊗ ε0 ⊗ ℏ ⊗ speedOfLight)
 
 -- | Mass of the proton.
 protonMass ∷ Quantity
