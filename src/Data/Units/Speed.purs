efb77fc554872db999dc02d2f3abc4314526e1b6
Nuno Marques <nunommarques@ua.pt>
2019-02-23 17:37:45 +0000

    Fix last import statement

--- src/Data/Units/Speed.purs
+++ src/Data/Units/Speed.purs
@@ -1,7 +1,6 @@
 module Data.Units.Speed where
 
-import Prelude ((/))
-import Data.Units(DerivedUnit, makeNonStandard)
+import Data.Units(DerivedUnit, makeNonStandard, (./))
 import Data.Units.SI
 
 knot :: DerivedUnit
